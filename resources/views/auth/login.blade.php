232425e27a0f4a63529a17905bd1117692cc61e2
C4lumny <ospinonathan@gmail.com>
2024-06-24 16:36:32 -0500

    feat: :zap: Login - Signup finalizados. Ahora se muestra que no hay tareas en caso de que no hayan

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,10 +5,19 @@
     <form method="POST" action="{{ route('login') }}">
         @csrf
 
+        <div class="text-2xl text-gray-700 dark:text-gray-300 mt-4 mb-2 font-bold">
+            Sign In
+        </div>
+
+        <div class="text-md text-gray-700 dark:text-gray-300 mb-6">
+            ¡Si no tienes una cuenta, registrate! <br> Hazlo <a href="{{route('register')}}" class="text-white underline cursor-pointer">Aqui</a>
+        </div>
+
         <!-- Email Address -->
         <div>
             <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required
+                autofocus autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
@@ -16,10 +25,8 @@
         <div class="mt-4">
             <x-input-label for="password" :value="__('Password')" />
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
+            <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required
+                autocomplete="current-password" />
 
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
@@ -27,14 +34,17 @@
         <!-- Remember Me -->
         <div class="block mt-4">
             <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800" name="remember">
+                <input id="remember_me" type="checkbox"
+                    class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800"
+                    name="remember">
                 <span class="ms-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Remember me') }}</span>
             </label>
         </div>
 
         <div class="flex items-center justify-end mt-4">
             @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
+                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
+                    href="{{ route('password.request') }}">
                     {{ __('Forgot your password?') }}
                 </a>
             @endif
--- resources/views/todo.blade.php
+++ resources/views/todo.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="es">
+<html lang="en">
 
 <head>
     <meta charset="UTF-8">
@@ -17,12 +17,11 @@
         <span class="text-[#4EA8DE]">ToDo</span><span class="text-[#8284FA]">Master</span>
     </div>
 
-    <form action="{{ route('logout') }}" method="POST">
+    <form method="POST" action="{{ route('logout') }}" class="absolute right-0 top-0 mt-4 mr-4">
         @csrf
-        @method('POST')
-        <button>
-            Logout
-        </button>
+        <x-danger-button type="submit" class="text-red-500">
+            {{ __('Logout') }}
+        </x-danger-button>
     </form>
 
     {{-- Create a new task ✅ --}}
@@ -33,8 +32,9 @@
 
         <input name="title" type="text" placeholder="Añada una nueva tarea"
             class="bg-[#262626] rounded-lg p-4 w-full border-none focus:outline-none">
-        <button class="p-4 flex gap-2 rounded-lg bg-[#1E6F9F] items-center hover:bg-[#4EA8DE]">
-            Crear
+        <button
+            class="py-4 px-5 flex gap-2 items-center justify-center rounded-lg bg-[#1E6F9F] items-center hover:bg-[#4EA8DE]">
+            <span>Crear</span>
             <img src="{{ asset('icons/plus.svg') }}" alt="">
         </button>
     </form>
@@ -84,6 +84,15 @@
             </div>
             {{-- ToDo card 🗃️ --}}
             <div class="w-full my-6 space-y-5">
+                @if (count($todos) == 0)
+                    <div class="flex flex-col justify-center items-center mt-4 text-center">
+                        <img src="{{ asset('icons/Clipboard.svg') }}" alt="">
+
+                        <span class="mt-5 text-[#808080] font-bold">¡Ops! Parece que aún no tienes tareas
+                            registradas</span>
+                        <span class="text-[#808080]">Crea tareas y organiza tus pendientes</span>
+                    </div>
+                @endif
                 @foreach ($todos as $todo)
                     <div class="flex justify-between items-center bg-[#262626] p-4 rounded-lg">
                         <form action="{{ route('todos.update', $todo->id) }}" method="POST">
